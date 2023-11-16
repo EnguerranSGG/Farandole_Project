cbd4262e5100b6cff06d94a47c48865079b4e258
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2023-11-16 14:11:19 +0100

    Add services

---
+++ src/services/switch.arrows.js
@@ -0,0 +1,71 @@
+export function switchOver1() {
+    const up1 = document.getElementById('up1');
+    const down1 = document.getElementById('down1');
+    if (up1 && down1) {
+        up1.style.display = 'block';
+        down1.style.display = 'none';
+    }
+}
+
+export function switchOut1() {
+    const up1 = document.getElementById('up1');
+    const down1 = document.getElementById('down1');
+    if (up1 && down1) {
+        up1.style.display = 'none';
+        down1.style.display = 'block';
+    }
+}
+
+export function switchOver2() {
+    const up2 = document.getElementById('up2');
+    const down2 = document.getElementById('down2');
+    if (up2 && down2) {
+        up2.style.display = 'block';
+        down2.style.display = 'none';
+    }
+}
+
+export function switchOut2() {
+    const up2 = document.getElementById('up2');
+    const down2 = document.getElementById('down2');
+    if (up2 && down2) {
+        up2.style.display = 'none';
+        down2.style.display = 'block';
+    }
+}
+
+export function switchOver3() {
+    const up3 = document.getElementById('up3');
+    const down3 = document.getElementById('down3');
+    if (up3 && down3) {
+        up3.style.display = 'block';
+        down3.style.display = 'none';
+    }
+}
+
+export function switchOut3() {
+    const up3 = document.getElementById('up3');
+    const down3 = document.getElementById('down3');
+    if (up3 && down3) {
+        up3.style.display = 'none';
+        down3.style.display = 'block';
+    }
+}
+
+export function switchOver4() {
+    const up4 = document.getElementById('up4');
+    const down4 = document.getElementById('down4');
+    if (up4 && down4) {
+        up4.style.display = 'block';
+        down4.style.display = 'none';
+    }
+}
+
+export function switchOut4() {
+    const up4 = document.getElementById('up4');
+    const down4 = document.getElementById('down4');
+    if (up4 && down4) {
+        up4.style.display = 'none';
+        down4.style.display = 'block';
+    }
+}
---
+++ src/services/utilitaires.js
@@ -0,0 +1,90 @@
+export const isEmpty = (value) => {
+  return (
+    value === undefined ||
+    value === null ||
+    (typeof value === 'object' && Object.keys(value).length === 0) ||
+    (typeof value === 'string' && value.trim().length === 0))
+}
+
+export const voirDeguisements = () => {
+  const produits = document.getElementById('deguisements');
+  const instruction = document.getElementById('instruction_deguisements');
+
+  if (produits.className === 'pasProduits') {
+    produits.className = 'produits';
+    instruction.textContent = 'Cacher tous les déguisements';
+  } else {
+    produits.className = 'pasProduits';
+    instruction.textContent = 'Voir tous nos déguisements';
+  }
+}
+
+export const voirAccessoires = () => {
+  const produits = document.getElementById('accessoires');
+  const instruction = document.getElementById('instruction_accessoires');
+
+  if (produits.className === 'pasAccessoires') {
+    produits.className = 'produits';
+    instruction.textContent = 'Cacher tous les accessoires';
+  } else {
+    produits.className = 'pasAccessoires';
+    instruction.textContent = 'Voir tous nos accessoires';
+  }
+}
+
+export const viewerProduits = (produit) => {
+  const viewer = document.getElementById('viewer');
+  viewer.className = 'viewer';
+
+  viewer.innerHTML = `
+  <div>
+    <img src="${produit.image}" alt="${produit.nom}" id="viewer_img"/>
+    <p>${produit.nom}</p>
+    <button class="btn" id="fermer_deguisements">Fermer</button>
+  </div>`
+
+  const btn = document.getElementById('fermer_deguisements');
+
+  btn.addEventListener('click', () => {
+    viewer.className = 'notViewer';
+  })
+}
+
+export const viewerAccessoires = (produit) => {
+  const viewer = document.getElementById('viewer_accessoires');
+  viewer.className = 'viewer';
+
+  viewer.innerHTML = `
+    <div>
+      <img src="${produit.image}" alt="${produit.nom}"/>
+      <p>${produit.nom}</p>
+      <button class="btn" id="fermer_accessoires">Fermer</button>
+    </div>`
+
+  const btn = document.getElementById('fermer_accessoires');
+
+  btn.addEventListener('click', () => {
+    viewer.className = 'notViewer';
+  })
+}
+
+export const viewerChateaux = (produit) => {
+  const viewer = document.getElementById('viewer_accessoires');
+  viewer.className = 'viewerChateaux';
+
+  viewer.innerHTML = `
+    <div>
+    <h2>${produit.nom}</h2>
+      <div id="images_chateaux"><img id="image1_chateaux" src="${produit.image1}" alt="${produit.nom}"/>
+      <img id="image2_chateaux" src="${produit.image2}" alt="${produit.nom}"/>
+      <img id="image3_chateaux" src="${produit.image3}" alt="${produit.nom}"/></div>
+      <p>${produit.dimensions}</p>
+      <button class="btn" id="fermer_accessoires">Fermer</button>
+    </div>`
+
+  const btn = document.getElementById('fermer_accessoires');
+
+  btn.addEventListener('click', () => {
+    viewer.className = 'notViewer';
+  })
+}
