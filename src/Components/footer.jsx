70f22822b6d415c36970a0a605e27b1ee9283662
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2023-11-17 17:12:32 +0100

    Add contact links on footer

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import Facebook from "../Illustrations/facebook-logo.svg";
 import { NavLink } from "react-router-dom";
 
 function Footer() {
     return (
         <footer>
-            <div>
-                <NavLink to="/Contact">Nous contacter</NavLink>
-                <a href="https://www.facebook.com/profile.php?id=100090708152032"></a>
+            <div class="liens_contact">
+                <div id="contact"><NavLink to="/Contact">Nous contacter</NavLink></div>
+                <div id="facebook"><a href="https://www.facebook.com/profile.php?id=100090708152032"><img src={Facebook} alt="Liens versFacebook"></img></a></div>
             </div>
             <div>
                 <p>©2023LaFarandole. Tous droits reservés.</p>
