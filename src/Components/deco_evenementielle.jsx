9ad34fdd5585366a90532a03bdb59c61362ddc9f
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2023-11-22 11:24:12 +0100

    Create a deco component

---
+++ src/Components/deco_evenementielle.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function Deco() {
+    return (
+        <>
+            <section className='presentation_deco'>
+                <div>
+                    <h1>Vous organisez un événement ?</h1>
+                    <p>Que ce soit pour la décoration d'une table, d'une salle ou d'un extérieur, nous pouvons vous aider. <br /> Nos services vont de... Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem,i molestias! Error, animi placeat sit quo repellat itaque consequuntur dolore facere totam enim harum.
+                    </p>
+                    <p>Alors contactez-nous au <strong>09 82 65 07 48 </strong> ou par mail à <strong>lafarandole.avesne@gmail.com</strong>!</p>
+                    <p>Vous pouvez aussi venir directement à notre rencontre dans notre boutique au <strong>92 Grande Rue à Avesnes-le-Comte !</strong></p></div></section></>
+    )
+}
+
+export default Deco;
