bed70e04c11bad05a36d638ef38dd38304b7ff45
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2023-11-22 11:23:31 +0100

    Create an error component

---
+++ src/Components/error.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Error() {
+    return (
+        <>
+            <section className='error'>
+                <div>
+                    <h1 id="alerte">⚠</h1>
+                    <h1>Désolé mais cette page n'existe pas.</h1>
+                </div>
+            </section></>
+    )
+}
+
+export default Error;
