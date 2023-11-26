f332d3725848158efae8289c875345410f2db388
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2023-11-26 12:19:50 +0100

    Supression of a useless btn's className

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -6,9 +6,9 @@ function Footer() {
     return (
         <footer>
             <div className="liens_contact">
-                <div id="contact"><NavLink to="/Contact" className="btn_contact">Nous contacter</NavLink></div>
+                <div id="contact"><NavLink to="/Contact">Nous contacter</NavLink></div>
                 <div id="facebook"><a href="https://www.facebook.com/profile.php?id=100090708152032"><img src={Facebook} alt="Liens versFacebook"></img></a></div>
-                <div id="contact"><NavLink to="/Mentions_legales" className="btn_contact">Mentions légales</NavLink></div>
+                <div id="contact"><NavLink to="/Mentions_legales">Mentions légales</NavLink></div>
             </div>
             <div>
                 <p>©2023LaFarandole. Tous droits reservés.</p>
