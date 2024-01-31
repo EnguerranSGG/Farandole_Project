8b5bc938ac5a90a06bc3c0fcf68b1c465d5f3209
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-01-31 11:44:55 +0100

    Modifications on "Header_home" 's presentation

--- src/Components/header_home.jsx
+++ src/Components/header_home.jsx
@@ -49,10 +49,9 @@ function HeaderHome() {
             </nav>
             <div id='banniere_droite' />
             <div className='bienvenue'>
-                <div>
-                <h1>Vous organisez un événement ? </h1>
+                <div className='bienvenue_text'>
+                <h1>Vous organisez un batême ? Une communion ? Un mariage ? Un anniversaire ? </h1>
                 <h2>Un événement important pour <strong>vous</strong> est aussi important pour <strong>nous</strong>. </h2>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, eligendi. Vero error id cupiditate animi. Sed modi, odit veritatis earum nostrum labore consectetur fugiat minima officia hic nisi saepe quidem.</p>
                 <section className='contact_home'><NavLink to="/Contact" className="btn_contact_home">Nous contacter</NavLink></section>
                 </div>
             </div>
