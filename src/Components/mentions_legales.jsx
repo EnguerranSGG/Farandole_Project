6e4aa09c2482b0c53c5ff2d6987520a335ef3ade
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-02-07 21:49:23 +0100

    Finalyse legal mentions

--- src/Components/mentions_legales.jsx
+++ src/Components/mentions_legales.jsx
@@ -10,14 +10,14 @@ function Mentions() {
                     <p>Adresse mail : lafarandole.avesne@gmail.com.</p>
                     <p>Numéro de téléphone : 09 82 65 07 48.</p>
                     <p>RCS : A418322830.</p>
-                    <p>N° TVA : 0000000000000.</p>
-                    <p>Site herbrgé par "???" (adresse : "???" )</p>
+                    <p>Site herbrgé par "Hostinger" (adresse : "HOSTINGER INTERNATIONAL LTD, 61 Lordou Vironos Street, 6023 Larnaca, Chypre" )</p>
+                    <p>Site réalisé par <a href="https://www.linkedin.com/in/enguerran-smagghe/">Enguerran Smagghe</a>.</p>
 
                     <h2>Données personnelles</h2>
                     <p>Conformément à l’article 34 de la loi n°78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, vous disposez d’un droit d’accès, de modification, de rectification et de suppression des données nominatives vous concernant. Vous pouvez exercer ce droit par e-mail à : lafarandole.avesne@gmail.com</p>
 
                     <h2>Description des services fournis</h2>
-                    <p> Le site www.laFarandole.com a pour but de fournir des informations concernant l’ensemble des produits et services de la société. Le propriétaire du site s’efforce de fournir sur le site www.laFarandole.com des informations aussi précises que possible.
+                    <p> Le site www.lafarandole-avesnes.fr a pour but de fournir des informations concernant l’ensemble des produits et services de la société. Le propriétaire du site s’efforce de fournir sur le site www.laFarandole.com des informations aussi précises que possible.
 
                         Toutefois, il ne pourra être tenue responsable des omissions, des inexactitudes et des carences dans la mise à jour. Toutes les informations proposées sur le site www.laFarandole.com sont données à titre indicatif, sont non exhaustives, et sont susceptibles d’évoluer. Elles sont données sous réserve de modifications ayant été apportées depuis leur mise en ligne.</p>
                 </div>
