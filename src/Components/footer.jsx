8f1628b4ff6777fc6b1523a595e26f8c7f08f935
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-02-08 17:32:58 +0100

    Add scroll to top for contact and mentions link

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -6,9 +6,9 @@ function Footer() {
     return (
         <footer>
             <div className="liens_contact">
-                <div id="contact"><NavLink to="/Contact">Nous contacter</NavLink></div>
+                <div id="contact"><NavLink to="/Contact" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>Nous contacter</NavLink></div>
                 <div id="facebook"><a href="https://www.facebook.com/profile.php?id=100090708152032"><img src={Facebook} alt="Liens versFacebook"></img></a></div>
-                <div id="contact"><NavLink to="/Mentions_legales">Mentions légales</NavLink></div>
+                <div id="contact"><NavLink to="/Mentions_legales" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>Mentions légales</NavLink></div>
             </div>
             <div>
                 <p>©2023LaFarandole. Tous droits reservés.</p>
